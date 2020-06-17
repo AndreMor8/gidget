0e2224dc1409fa8e875732d841dc7fe2b6348e16
AndreMor955 <moralesandre75@gmail.com>
2020-06-17 02:37:18 -0500

    Update prefix.js

--- src/database/models/prefix.js
+++ src/database/models/prefix.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
     guildId: { type: String, required: true },
-    prefix: { type: String, required: true, default: "g%" }
+    prefix: { type: String, required: true }
 });
 
 const MessageModel = module.exports = mongoose.model('prefix', MessageSchema);
