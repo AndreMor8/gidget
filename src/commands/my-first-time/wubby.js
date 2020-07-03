c3c791ac9289adb1b97a2d5b4b7ec888657dcdd5
Monoverde888 <minecraftersov@gmail.com>
2020-07-02 21:46:21 -0400

    Update wubby.js

--- src/commands/my-first-time/wubby.js
+++ src/commands/my-first-time/wubby.js
@@ -1,3 +1,4 @@
+//Note that if you dont like this command you can delete it safely because i made this when i was new to discordjs and it is not neccesary to the bot
 module.exports = {
     run: async (bot, message, args) => {
       message.channel.send("<:Wubby:665434218163208192>")
