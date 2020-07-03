0c4b27c9fec6f58a7c1bee53b24ddd2cd352858f
Monoverde888 <minecraftersov@gmail.com>
2020-07-02 21:45:25 -0400

    Update widgetfix.js

--- src/commands/my-first-time/widgetfix.js
+++ src/commands/my-first-time/widgetfix.js
@@ -1,3 +1,4 @@
+//Note that if you dont like this command you can delete it safely because i made this when i was new to discordjs and it is not neccesary to the bot
 module.exports = {
     run: async (bot, message, args) => {
       let widget = ["Oops, that's not supposed to happen", "No problemo! <:WidgetWalk:610311126193930240>", "Oops, that wasn't supposed to happen", "I can fix anything <:WidgetFix:626764896184434690>"];
