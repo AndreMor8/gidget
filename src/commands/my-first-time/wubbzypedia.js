be957ac92a9ccb897623b9356095448a98e1a9ae
Monoverde888 <minecraftersov@gmail.com>
2020-07-02 21:47:05 -0400

    Update wubbzypedia.js

--- src/commands/my-first-time/wubbzypedia.js
+++ src/commands/my-first-time/wubbzypedia.js
@@ -1,3 +1,4 @@
+//Note that if you dont like this command you can delete it safely because i made this when i was new to discordjs and it is not neccesary to the bot
 module.exports = {
     run: async (bot, message, args) => {
       if(!message.guild) return message.channel.send('This command only works on Wow Wow Discord.')
