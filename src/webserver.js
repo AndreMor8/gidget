a89b74f329c2c5b145d4c835ed413adf61b5f3ae
AndreMor955 <moralesandre75@gmail.com>
2020-07-04 01:33:26 -0500

    :)

--- src/webserver.js
+++ src/webserver.js
@@ -1,13 +1,15 @@
 const { bot } = require('./index.js');
+const http = require("http");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", async (req, res) => {
-  console.log(req.headers);
-  if(req.query && req.query.delete) {
-    console.log(req.query);
+  if (!req.headers) return res.status(403).send("You don't have authorization");
+  if (req.headers.pass !== process.env.SECRET) return res.status(403).send("You don't have authorization");
+  if(req.query && req.query.guild) {
+    deleteCache(req.query.guild);
   }
   res.status(200).send("Good");
 });
@@ -16,7 +18,11 @@ const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-function deleteCache(bot, guildID) {
+setInterval(() => {
+  http.get("http://" + process.env.PROJECT_DOMAIN + "/");
+}, 90000);
+
+function deleteCache(guildID) {
   bot.cachedMessageReactions.delete(guildID);
   bot.autoresponsecache.delete(guildID);
   bot.level.delete(guildID);
