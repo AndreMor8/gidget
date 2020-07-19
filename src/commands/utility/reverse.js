e2029b971e000e9c9d79f2f515984409b6ef0319
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-07-19 01:07:28 -0500

    Update reverse.js

--- src/commands/utility/reverse.js
+++ src/commands/utility/reverse.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     run: async (bot, message, args) => {
         if(!args[1]) return message.reply('you must input text to be reversed!')
-        message.channel.send(args.slice(1).join(' ').split('').reverse().join(''), { disableMentions: "all" });
+        message.channel.send(args.slice(1).join(' ').split('').reverse().join(''));
     },
     aliases: [],
     description: "Reverse some text",
