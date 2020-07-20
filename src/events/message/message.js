4ad43d1b356a9751ddbe1cd69912f06bf815bff4
AndreMor955 <moralesandre75@gmail.com>
2020-07-19 19:53:29 -0500

    :)

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const badwords = require("badwords.js");
+const towork = new badwords();
 const MessageModel = require("../../database/models/customresponses");
 const MessageModel2 = require("../../database/models/levelconfig");
 const MessageModel3 = require("../../database/models/prefix");
@@ -19,7 +21,7 @@ module.exports = async (bot, message = new Discord.Message(), nolevel = false) =
     }
     if (msgDocument) {
       const { responses } = msgDocument;
-      if(responses) {
+      if (responses) {
         const arr = Object.entries(responses);
         for (let i in arr) {
           const regex = new RegExp(arr[i][0], "gmi");
@@ -31,51 +33,60 @@ module.exports = async (bot, message = new Discord.Message(), nolevel = false) =
     }
 
     // Level system
-    if(!nolevel) {
+    if (!nolevel) {
       let msgDocument2 = bot.level.get(message.guild.id);
-    if (!msgDocument2) {
-      msgDocument2 = await MessageModel2.findOne({ guildId: message.guild.id });
-      bot.level.set(message.guild.id, msgDocument2);
-    }
-    if (msgDocument2 && msgDocument2.levelsystem) {
-      if (!timer.get(message.author.id)) {
-        timer.set(message.author.id, true);
-        setTimeout(() => {
-          timer.delete(message.author.id);
-        }, 120000);
-        const randomAmountOfXp = Math.floor(Math.random() * 9) + 1; // Min 1, Max 10
-        const hasLeveledUp = await Levels.appendXp(
-          message.author.id,
-          message.guild.id,
-          randomAmountOfXp
-        );
-        if (hasLeveledUp) {
-          const user = await Levels.fetch(message.author.id, message.guild.id);
-          let {roles} = msgDocument2;
-          if (roles[user.level - 1]) {
-            let toadd = roles[user.level - 1].filter(e => message.guild.roles.cache.has(e) && message.guild.roles.cache.get(e).editable && !message.guild.roles.cache.get(e).managed)
-            message.member.roles.add(toadd);
+      if (!msgDocument2) {
+        msgDocument2 = await MessageModel2.findOne({ guildId: message.guild.id });
+        bot.level.set(message.guild.id, msgDocument2);
+      }
+      if (msgDocument2 && msgDocument2.levelsystem) {
+        if (!timer.get(message.author.id)) {
+          timer.set(message.author.id, true);
+          setTimeout(() => {
+            timer.delete(message.author.id);
+          }, 120000);
+          const randomAmountOfXp = Math.floor(Math.random() * 9) + 1; // Min 1, Max 10
+          const hasLeveledUp = await Levels.appendXp(
+            message.author.id,
+            message.guild.id,
+            randomAmountOfXp
+          );
+          if (hasLeveledUp) {
+            const user = await Levels.fetch(message.author.id, message.guild.id);
+            let { roles } = msgDocument2;
+            if (roles[user.level - 1]) {
+              let toadd = roles[user.level - 1].filter(e => message.guild.roles.cache.has(e) && message.guild.roles.cache.get(e).editable && !message.guild.roles.cache.get(e).managed)
+              message.member.roles.add(toadd);
+            }
           }
-        }
-        if (hasLeveledUp && msgDocument2.levelnotif) {
-          const user = await Levels.fetch(message.author.id, message.guild.id);
-          message.channel.send(
-            `${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`
-          ).catch(err => {});
+          if (hasLeveledUp && msgDocument2.levelnotif) {
+            const user = await Levels.fetch(message.author.id, message.guild.id);
+            message.channel.send(
+              `${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`
+            ).catch(err => { });
+          }
         }
       }
     }
   }
+
+
+  //Things for Wow Wow Discord
+  if (message.guild.id === process.env.GUILDID) {
+    if (towork.isProfane(message.content)) {
+      await message.delete()
+      await message.reply("swearing is not allowed in this server!", { allowedMentions: { parse: ["users"] } });
     }
+  }
 
   // Command handler and custom prefix. Check that the channel is not a DM to check the prefix.
 
   let PREFIX;
   if (message.guild) {
     let prefixes = bot.guildprefix.get(message.guild.id);
-    if(!prefixes) {
+    if (!prefixes) {
       let pr = await MessageModel3.findOne({ guildId: message.guild.id })
-      if(!pr) {
+      if (!pr) {
         PREFIX = "g%";
         bot.guildprefix.set(message.guild.id, "g%");
       } else {
@@ -89,25 +100,25 @@ module.exports = async (bot, message = new Discord.Message(), nolevel = false) =
     console.log(message.author.tag + ' "' + message.content + '"');
     PREFIX = "g%";
   }
-  if(message.mentions.users.get(bot.user.id) && message.content.startsWith("<@")) message.channel.send("My prefix in this server is " + PREFIX);
+  if (message.mentions.users.get(bot.user.id) && message.content.startsWith("<@")) message.channel.send("My prefix in this server is " + PREFIX);
   if (!message.content.startsWith(PREFIX)) return;
   let args = message.content.substring(PREFIX.length).split(/ +/g);
   //let args = message.content.slice(PREFIX.length).trim().split(/ +/g);
   if (!args[0]) return;
   const command = bot.commands.get(args[0].toLowerCase()) || bot.commands.find(a => a.aliases.includes(args[0].toLowerCase()))
   if (command) {
-    if(message.guild && message.channel.permissionsFor(bot.user).has("SEND_MESSAGES")) {
+    if (message.guild && message.channel.permissionsFor(bot.user).has("SEND_MESSAGES")) {
       command.run(bot, message, args)
-      .catch(err => {
-        console.error(err);
-        message.channel.send("Something happened! Here's a debug: " + err).catch(err => {});
-      });
-    } else if(!message.guild) {
+        .catch(err => {
+          console.error(err);
+          message.channel.send("Something happened! Here's a debug: " + err).catch(err => { });
+        });
+    } else if (!message.guild) {
       command.run(bot, message, args)
-      .catch(err => {
-        console.error(err);
-        message.channel.send("Something happened! Here's a debug: " + err).catch(err => {});
-      });
+        .catch(err => {
+          console.error(err);
+          message.channel.send("Something happened! Here's a debug: " + err).catch(err => { });
+        });
     }
   }
 };
