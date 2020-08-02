878eeba7f8f1e616c09c56cec95ce4b356ce29fa
AndreMor955 <moralesandre75@gmail.com>
2020-08-02 16:20:42 -0500

    Small update

--- src/commands/owner/exec.js
+++ src/commands/owner/exec.js
@@ -1,14 +1,16 @@
 const { execSync } = require("child_process");
-
+const { Util } = require("discord.js");
 module.exports = {
     run: async (client, message, args) => {
         if (message.author.id !== "577000793094488085") return message.channel.send("Only AndreMor can use this command");
         try {
             let process = execSync(args.slice(1).join(" "));
             if (!process.toString()) return message.channel.send("Command executed, but no output");
-            message.channel.send(process, { code: "sh" });
+            const text = Util.splitMessage(process.toString());
+            message.channel.send(text[0], { code: "sh" });
         } catch (error) {
-            message.channel.send(error, { code: "sh" });
+            const text = Util.splitMessage(error.toString());
+            message.channel.send(text[0], { code: "sh" });
         }
     },
     aliases: [],
