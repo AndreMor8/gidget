a2d08710117e6239c8c2c1e524365f1aeb28ebce
AndreMor955 <moralesandre75@gmail.com>
2020-08-02 12:21:03 -0500

    :)

--- src/webserver.js
+++ src/webserver.js
@@ -68,10 +68,6 @@ const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-setInterval(() => {
-  http.get("http://" + process.env.PROJECT_DOMAIN + "/");
-}, 90000);
-
 function deleteCache(guildID) {
   bot.cachedMessageReactions.delete(guildID);
   bot.autoresponsecache.delete(guildID);
