f16cf61b1cba1af742a40275756b9c400f0d6d72
AndreMor955 <moralesandre75@gmail.com>
2020-08-06 14:28:00 -0500

    Yes

--- src/commands/bot-interact/servers.js
+++ src/commands/bot-interact/servers.js
@@ -1,9 +1,11 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
     run: async (bot, message, args) => {
         let servers = bot.guilds.cache.size
         let users = bot.users.cache.size
 
-        let serverEmbed = new Discord.MessageEmbed()
+        let serverEmbed = new MessageEmbed()
             .setDescription("At the moment I'm in **" + servers + "** servers and with **" + users + "** online users.", true)
             .setColor(0xf7a7ff)
             .setFooter('Requested by ' + message.author.username, message.author.displayAvatarURL());
