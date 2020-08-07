a6a3f8a80854bbdd6c6fc2d7c0d70d42fb7a06ad
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-08-07 16:19:31 -0500

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,6 +1,7 @@
 const ytdl = require("ytdl-core");
 const ytsr = require("ytsr");
 const ytpl = require("ytpl");
+ytpl.do_warn_deprecate = false
 const moment = require("moment");
 require("moment-duration-format");
 module.exports = {
@@ -73,7 +74,7 @@ module.exports = {
       }
       message.channel.startTyping();
       return handleVideo(message, voiceChannel, "https://www.youtube.com/watch?v=" + args[1]);
-    } else if (ytpl.validateID(args[1])) {
+    } else if (ytpl.validateURL(args[1])) {
       let form1 = await message.channel.send("Hang on! <:WaldenRead:665434370022178837>");
       message.channel.startTyping();
       try {
