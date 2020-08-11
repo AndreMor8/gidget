9da2942ea1d240ad3e41014169355d661f3325d8
AndreMor955 <moralesandre75@gmail.com>
2020-08-11 17:42:44 -0500

    Premium type

--- src/database/models/DiscordUser.js
+++ src/database/models/DiscordUser.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true },
     username: { type: String, required: true },
-    guilds: { type: Array, required: true }
+    guilds: { type: Array, required: true },
+    avatar: { type: String },
+    premium_type: { type: Number }
 });
 
-const DiscordUser = module.exports = mongoose.model('User', UserSchema);
+const DiscordUser = module.exports = mongoose.model('User', UserSchema);s
