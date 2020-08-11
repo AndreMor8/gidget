5d478d53811330c8e4357a7a49482bc6128a99a2
AndreMor955 <moralesandre75@gmail.com>
2020-08-11 02:40:56 -0500

    Yes

--- src/commands/owner/exec.js
+++ src/commands/owner/exec.js
@@ -1,16 +1,24 @@
-const { execSync } = require("child_process");
+const ch = require("child_process");
+const { promisify } = require("util");
+const exec = promisify(ch.exec);
 const { Util } = require("discord.js");
 module.exports = {
     run: async (client, message, args) => {
         if (message.author.id !== "577000793094488085") return message.channel.send("Only AndreMor can use this command");
         try {
-            let process = execSync(args.slice(1).join(" "));
-            if (!process.toString()) return message.channel.send("Command executed, but no output");
-            const text = Util.splitMessage(process.toString());
-            message.channel.send(text[0], { code: "sh" });
+            const { stdout, stderr } = await exec(args.slice(1).join(" "));
+            if (!stdout && !stderr) return message.channel.send("Command executed, but no output");
+            if(stdout) {
+                const text = Util.splitMessage(stdout);
+                await message.channel.send(text[0], { code: "sh" });
+            }
+            if(stderr) {
+                const text = Util.splitMessage(stderr);
+                await message.channel.send(text[0], { code: "sh" });
+            }
         } catch (error) {
             const text = Util.splitMessage(error.toString());
-            message.channel.send(text[0], { code: "sh" });
+            await message.channel.send(text[0], { code: "sh" });
         }
     },
     aliases: [],
