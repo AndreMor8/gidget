0c32034049de1d1e0d7aca5fe91c0166d3d22871
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-08-13 09:55:46 -0500

    Update index.js

--- src/utils/clean-url/index.js
+++ src/utils/clean-url/index.js
@@ -7,12 +7,12 @@ const data = require('./assets/sites.json');
 function checkCleanUrl(urlFromReq) {
     let parsedUrl = url.parse(urlFromReq);
     let host = parsedUrl.host || parsedUrl.pathname.trim().split("/")[0];
-    if(!host.startsWith('www.')) {
-        host = `www.${host}`;
-    }
-    if(host in data) {
-        return true;    
-    }
+    if(host in data) return true;
+    let thing = host.split(".");
+    let check = "www." + thing.slice(1).join(".");
+    if(check in data) return true;
+    if(!host.startsWith('www.')) host = `www.${host}`;
+    if(host in data) return true;
     return false;
 }
 
