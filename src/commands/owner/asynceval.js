1e936ea45fb4ed35288d94fe3d7ddab47bfb2b7e
AndreMor955 <moralesandre75@gmail.com>
2020-08-13 23:32:57 -0500

    :)

--- src/commands/owner/asynceval.js
+++ src/commands/owner/asynceval.js
@@ -5,7 +5,7 @@ module.exports = {
     if (message.author.id !== "577000793094488085") return message.channel.send('Only AndreMor can use this command')
     if (!args[1]) return message.channel.send("Put something to evaluate.\nThis allows using async/await without complications\nRemember to put `return` or this will not return anything.");
     try {
-      const evaluated = await eval("(async () => { " + args.slice(1).join(" ") + "})();");
+      let evaluated = await eval("(async () => { " + args.slice(1).join(" ") + "})();");
       if (typeof evaluated !== "string") evaluated = require("util").inspect(evaluated, { depth: 0 });
       const arr = Util.splitMessage(evaluated, { maxLength: 1950 });
       message.channel.send(arr[0], { code: "js" });
--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -6,7 +6,7 @@ module.exports = {
     if (message.author.id !== "577000793094488085") return message.channel.send('Only AndreMor can use this command')
     if (!args[1]) return message.channel.send("Put something to evaluate.");
     try {
-      const evaluated = await eval(args.slice(1).join(" "));
+      let evaluated = await eval(args.slice(1).join(" "));
       if (typeof evaluated !== "string") evaluated = require("util").inspect(evaluated, { depth: 0 });
       const arr = Util.splitMessage(evaluated, { maxLength: 1950 });
       message.channel.send(arr[0], { code: "js" });
