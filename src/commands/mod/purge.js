5deffa267e3095c9a097599cb675bc70253d3ded
AndreMor955 <moralesandre75@gmail.com>
2020-08-16 03:23:57 -0500

    An advanced purge command

--- src/commands/mod/purge.js
+++ src/commands/mod/purge.js
@@ -1,16 +1,72 @@
+const Discord = require('discord.js')
 module.exports = {
   run: async (bot, message = new Discord.Message(), args) => {
     if (!message.guild) return message.channel.send('This command only works on servers.')
     if (!message.channel.permissionsFor(message.member).has("MANAGE_MESSAGES")) return message.reply(`You do not have permission to execute this command.`)
     if (!message.channel.permissionsFor(message.guild.me).has("MANAGE_MESSAGES")) return message.reply("I don't have permission to delete messages :(");
-    if (!args[1]) return message.reply('how many messages should I delete? Specify it.')
-    let number = parseInt(args[1]);
-
+    if (!args[1] || (isNaN(args[1]) && !args[2])) return message.reply('how many messages should I delete? Specify it.');
+    let number = args[2] ? parseInt(args[2]) : parseInt(args[1]);
+    
     if (!isNaN(number) && (number <= 100) && (number >= 1)) {
       await message.delete();
-      message.channel.bulkDelete(number, true).catch(err => {
-        message.channel.send("Some error ocurred. Here's a debug: " + err);
-      });
+      switch (args[1]) {
+        case 'users': {
+          if (!args[3]) return message.channel.send("Mention or put the ID of the people whom you want their messages to be deleted.\n`purge users <number> <mentions>`")
+          const authors = message.mentions.users.size ? message.mentions.users.keyArray() : args.slice(3);
+          const messages = await message.channel.messages.fetch({
+            limit: number
+          }, false);
+          messages.sweep(m => !authors.includes(m.author.id));
+          await message.channel.bulkDelete(messages, true);
+          let thing = await message.channel.send(messages.size + " messages were successfully deleted");
+          thing.delete({ timeout: 5000 });
+        }
+        break;
+        case 'bots': {
+          const messages = await message.channel.messages.fetch({
+            limit: number
+          }, false);
+          messages.sweep(m => !m.author.bot);
+          await message.channel.bulkDelete(messages, true);
+          let thing = await message.channel.send(messages.size + " messages were successfully deleted");
+          thing.delete({ timeout: 5000 });
+        }
+        break;
+        case 'attachments': {
+          const messages = await message.channel.messages.fetch({
+            limit: number
+          }, false);
+          messages.sweep(m => !m.attachments.first());
+          await message.channel.bulkDelete(messages, true);
+          let thing = await message.channel.send(messages.size + " messages were successfully deleted");
+          thing.delete({ timeout: 5000 });
+        }
+        break;
+        case 'embeds': {
+          const messages = await message.channel.messages.fetch({
+            limit: number
+          }, false);
+          messages.sweep(m => !m.embeds[0]);
+          await message.channel.bulkDelete(messages, true);
+          let thing = await message.channel.send(messages.size + " messages were successfully deleted");
+          thing.delete({ timeout: 5000 });
+        }
+        break;
+        case 'with': {
+          const messages = await message.channel.messages.fetch({
+            limit: number
+          }, false);
+          messages.sweep(m => !(new RegExp(args.slice(3).join(" "), "gmi").test(m.content)));
+          await message.channel.bulkDelete(messages, true);
+          let thing = await message.channel.send(messages.size + " messages were successfully deleted");
+          thing.delete({ timeout: 5000 });
+        }
+        break;
+        default: {
+          if(args[2]) return message.channel.send("Invalid mode!");
+          await message.channel.bulkDelete(number, true);
+        }
+      }
     } else {
       if (isNaN(number)) {
         message.channel.send('That isn\'t a number!');
