82cdf7e1acc2e575b6ce4300ce45645d2ed1cdd6
AndreMor955 <moralesandre75@gmail.com>
2020-08-20 00:51:32 -0500

    Ok.

--- src/commands/bot-interact/prefix.js
+++ src/commands/bot-interact/prefix.js
@@ -4,9 +4,7 @@ const fs = require('fs');
 
 module.exports = {
   run: async (bot, message, args) => {
-    if (!message.guild) return message.channel.send('This command only works on servers.')
     if (!args[1]) return message.channel.send('The actual prefix is ' + message.guild.cache.prefix ? message.guild.prefix : await message.guild.getPrefix());
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`You do not have permission to execute this command.`)
     if (args[2]) return message.channel.send('I\'m not compatible with spaces, sorry.')
 
     const thing = await message.guild.setPrefix(args[1]);
