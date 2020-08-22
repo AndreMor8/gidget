3223b5f7d649491d8aba3ee9d664f0f3da5b890e
AndreMor955 <moralesandre75@gmail.com>
2020-08-22 01:06:02 -0500

    Ok

--- src/commands/level/togglelevel.js
+++ src/commands/level/togglelevel.js
@@ -1,8 +1,7 @@
-const MessageModel = require("../../database/models/levelconfig");
 module.exports = {
   run: async (bot, message, args) => {
     if (!args[1]) return message.channel.send("Usage: `togglelevel <system/notif>`");
-    const reference = message.guild.cache.levelconfig ? message.guild.levelconfig : await message.guild.getLevelConfig();
+    const reference = await message.guild.getLevelConfig();
     switch (args[1]) {
       case "notif":
         await message.guild.changeLevelConfig("levelnotif", !reference.levelnotif)
