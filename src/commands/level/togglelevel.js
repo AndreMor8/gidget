9dfc5e4d70e1a75636cfa919efc747200313acd3
AndreMor955 <moralesandre75@gmail.com>
2020-08-22 01:10:04 -0500

    Ok

--- src/commands/level/togglelevel.js
+++ src/commands/level/togglelevel.js
@@ -1,11 +1,11 @@
 module.exports = {
   run: async (bot, message, args) => {
     if (!args[1]) return message.channel.send("Usage: `togglelevel <system/notif>`");
-    const reference = await message.guild.getLevelConfig();
+    const reference = message.guild.cache.levelconfig ? message.guild.levelconfig : await message.guild.getLevelConfig();
     switch (args[1]) {
       case "notif":
         await message.guild.changeLevelConfig("levelnotif", !reference.levelnotif)
-        message.channel.send(`Now the level-up notifications are: ${!reference.levelsystem ? "Enabled" : "Disabled"}`)
+        message.channel.send(`Now the level-up notifications are: ${!reference.levelnotif ? "Enabled" : "Disabled"}`)
       break;
       case "system": 
         await message.guild.changeLevelConfig("levelsystem", !reference.levelsystem)
