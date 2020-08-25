c15cfea9c9e3389f8233dabefdeed503ef967d1c
AndreMor955 <moralesandre75@gmail.com>
2020-08-25 09:54:45 -0500

    Ok

--- src/commands/owner/reboot.js
+++ src/commands/owner/reboot.js
@@ -1,7 +1,15 @@
-const Discord = require('discord.js');
-
+let thing = new Map();
 module.exports = {
     run: async (bot, message, args) => {
+        if(bot.voice.connections.size) {
+            if(!thing.get(message.author.id)) {
+                thing.set(message.author.id, true)
+                setTimeout(() => {
+                    thing.delete(message.author.id);
+                }, 15000);
+                return message.channel.send("There are users using the music function! If you do the command again the music will stop on them!");
+            }
+        }
         await message.channel.send('I\'m rebooting. Check the log to see if I\'m active.');
         process.exit(0);
     },
