78008d26d1fedae928424302c0deef49981cbbda
AndreMor955 <moralesandre75@gmail.com>
2020-09-02 21:19:42 -0500

    Yes

--- src/commands/music/record.js
+++ src/commands/music/record.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
 const { Readable } = require('stream');
-
+const Lame = require("node-lame").Lame;
 module.exports = {
   run: async (bot, message, args) => {
-    if(!args[1]) return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
+    if (!args[1]) return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
     let options = {}
-    if(args[1] === "dm" || args[1] === "server") {
+    if (args[1] === "dm" || args[1] === "server") {
       options = { mode: "pcm" }
-    } else if(args[1] !== "play") return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
+    } else if (args[1] !== "play") return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
     const serverQueue = message.guild.queue
     if (serverQueue && serverQueue.inseek) return;
     let musicVariables = message.guild.musicVariables;
@@ -37,13 +37,13 @@ module.exports = {
       let i = 0;
       let o = 0;
       connection.on("speaking", async (user, speaking) => {
-        if(user.id !== message.author.id) return;
+        if (user.id !== message.author.id) return;
         if (speaking.has("SPEAKING") && i < 1) {
           i++
           await message.channel.send('I am listening to you. Stop talking to give you the recording.');
-        } else if(i > 0 && o < 1) {
+        } else if (i > 0 && o < 1) {
           o++
-          if(args[1] === "play") {
+          if (args[1] === "play") {
             const dispatcher = connection.play(audio, { type: "opus" })
             dispatcher.on("start", () => {
               message.channel.send("I'm playing your audio.");
@@ -61,11 +61,35 @@ module.exports = {
               message.guild.musicVariables = null;
             })
           } else {
-            const attachment = new Discord.MessageAttachment(audio, 'audio.pcm');
-            if(args[1] === "server") await message.channel.send("Here's your recording. You must pass it to Audacity as raw data to listen to it.", attachment);
-            else await message.member.send("Here's your recording. You must pass it to Audacity as raw data to listen to it.", attachment);
-            await voiceChannel.leave();
-            message.guild.musicVariables = null;
+            message.channel.startTyping();
+            let chunks = [];
+            audio.on("data", (c) => {
+              chunks.push(c);
+            });
+            audio.on("end", () => {
+              let buf = Buffer.concat(chunks);
+              const encoder = new Lame({
+                "output": "buffer",
+                "bitrate": 192,
+                "raw": true,
+                "sfreq": 48,
+                "bitwidth": 16,
+                "signed": true,
+                "little-endian": true,
+              }).setBuffer(buf);
+              encoder.encode().then(async res => {
+                const buf = res.getBuffer();
+                const attachment = new Discord.MessageAttachment(buf, 'audio.mp3');
+                if (args[1] === "server") await message.channel.send("Here's your recording.", attachment);
+                else await message.member.send("Here's your recording.", attachment);
+              }).catch(async err => {
+                await message.channel.send("Error: " + err);
+              }).finally(async () => {
+                await voiceChannel.leave();
+                message.channel.stopTyping(true);
+                message.guild.musicVariables = null;
+              });
+            })
           }
         }
       });
