d153c8aad5da30a50fe3a4695c6432c98ed8f3f1
AndreMor955 <moralesandre75@gmail.com>
2020-09-03 02:57:06 -0500

    :)

--- src/commands/music/record.js
+++ src/commands/music/record.js
@@ -9,23 +9,16 @@ class Silence extends Readable {
 }
 module.exports = {
   run: async (bot, message, args) => {
+    if (!message.member.voice.channel) return message.channel.send("You have to be in a voice channel!");
     if (!args[1]) return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
     let options = {}
-    if (args[1] === "dm" || args[1] === "server") {
+    if (["dm", "server"].includes(args[1].toLowerCase())) {
       options = { mode: "pcm" }
-    } else if (args[1] !== "play") return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
-    const serverQueue = message.guild.queue
+    } else if (args[1].toLowerCase() !== "play") return message.channel.send("`dm` to send the file to your direct messages, `play` to play it on the voice channel, `server` to send the file to this channel.");
+    const serverQueue = message.guild.queue;
     if (serverQueue && serverQueue.inseek) return;
     let musicVariables = message.guild.musicVariables;
-    if (!message.member.voice.channel)
-      return message.channel.send(
-        "You have to be in a voice channel to pause the music!"
-      );
-    if (serverQueue || musicVariables) {
-      return message.channel.send(
-        "I am doing another operation."
-      );
-    }
+    if (serverQueue || musicVariables) return message.channel.send("I'm doing another operation.");
     if (musicVariables && musicVariables.other) return message.channel.send("I'm doing another operation");
     message.guild.musicVariables = { other: true };
     musicVariables = message.guild.musicVariables;
@@ -35,7 +28,7 @@ module.exports = {
       await message.channel.send('Start talking. I will record **what you say** until you finish speaking.');
       const audio = connection.receiver.createStream(message.author, options);
       let col = message.channel.createMessageCollector((m) => m.author.id === message.author.id && m.content.toLowerCase() === "stop");
-      col.on("collect", (m) => {
+      col.on("collect", () => {
         col.stop();
         audio.destroy();
         voiceChannel.leave()
@@ -48,13 +41,13 @@ module.exports = {
         if (speaking.has("SPEAKING") && i < 1) {
           i++
           col.stop();
-          await message.channel.send('I am listening to you. Stop talking to give you the recording.');
+          await message.channel.send('I\'m listening to you. Stop talking to give you the recording.');
         } else if (i > 0 && o < 1) {
           o++
           if (args[1] === "play") {
             const dispatcher = connection.play(audio, { type: "opus" })
             dispatcher.on("start", () => {
-              message.channel.send("I'm playing your audio.").catch(err => {});
+              message.channel.send("I'm playing your recording.").catch(err => {});
             })
             dispatcher.on("finish", async () => {
               await voiceChannel.leave();
@@ -65,7 +58,7 @@ module.exports = {
             })
             dispatcher.on("error", async (err) => {
               await voiceChannel.leave();
-              message.channel.send("Some error ocurred! Here's a debug: " + err).catch(err => {});
+              message.channel.send("Some error ocurred in the dispatcher! Here's a debug: " + err).catch(err => {});
               message.guild.musicVariables = null;
             })
           } else {
@@ -88,12 +81,22 @@ module.exports = {
               encoder.encode().then(async res => {
                 const buf = res.getBuffer();
                 const attachment = new Discord.MessageAttachment(buf, 'audio.mp3');
-                if (args[1] === "server") await message.channel.send("Here's your recording.", attachment);
+                if (args[1] === "server") await message.channel.send("Here's your recording.", attachment).catch(err => {});
                 else await message.member.send("Here's your recording.", attachment).catch(err => {
-                  message.channel.send("I can't send you DMs :(").catch(err => {});
+                  switch(err.code) {
+                    case 40005:
+                      message.channel.send("The file is very large. I can't send you that.").catch(err => {});
+                      break;
+                    case 50007:
+                      message.channel.send("I can't send you DMs :(").catch(err => {});
+                      break;
+                    default:
+                      console.log(err);
+                      message.channel.send("Error when sending the file: " + err).catch(err => {});
+                  }
                 });
               }).catch(async err => {
-                await message.channel.send("Error: " + err).catch(err => {});
+                await message.channel.send("Error when converting the PCM buffer: " + err).catch(err => {});
               }).finally(() => {
                 voiceChannel.leave();
                 message.channel.stopTyping(true);
