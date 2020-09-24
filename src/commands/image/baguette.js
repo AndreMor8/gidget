71449e20c19d4d20b3d02dbb07a3e9ec20784e7c
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-09-24 10:19:20 -0500

    Update baguette.js

--- src/commands/image/baguette.js
+++ src/commands/image/baguette.js
@@ -12,14 +12,16 @@ export default class extends Command {
         
         const msg = await message.channel.send("Generating... (this may take a while)")
         fetch(`https://nekobot.xyz/api/imagegen?type=baguette&url=${person.displayAvatarURL({dynamic: true, size: 1024})}`)
-        .then((res) => res.json())
-        .then((body) => {
+        .then((res) => {
+            if(res.ok) res.json().then((body) => {
             console.log(body)
             let embed = new MessageEmbed()
             .setTitle("Here ya go")
             .setImage(body.message)
             .setColor("RANDOM") 
             msg.edit(embed)
+        })
+            else return message.channel.send("Something happened with the third-party API")
         })
     }
 }
