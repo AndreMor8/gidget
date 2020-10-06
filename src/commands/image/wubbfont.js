930e4aa029e76e2f9e04db236e8a9d364e52e665
AndreMor955 <moralesandre75@gmail.com>
2020-10-05 19:22:43 -0500

    Yes

--- src/commands/image/wubbfont.js
+++ src/commands/image/wubbfont.js
@@ -18,11 +18,11 @@ export default class extends Command {
         message.channel.startTyping();
         if(!font) font = await Jimp.loadFont(path.join(__dirname, "/../../utils/", "font.fnt"));
         const image = new Jimp(WIDTH, HEIGHT);
-        image.print(font, 0, HEIGHT/10, {
+        image.print(font, 0, 0, {
             text: getWellText(args.slice(1).join(" "), 21, 120),
             alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
             alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-        }, WIDTH, HEIGHT/2);
+        }, WIDTH, HEIGHT);
         const buf = await image.getBufferAsync(Jimp.MIME_PNG);
         const att = new MessageAttachment(buf, "wubbtext.png");
         message.channel.stopTyping(true);
