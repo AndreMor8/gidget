fa7a948a8733244594b0879ccccf64522525e956
AndreMor955 <moralesandre75@gmail.com>
2020-10-16 17:50:54 -0500

    ok

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -15,13 +15,23 @@ export default class extends Command {
   async run(bot, message, args) {
     if (!args[1]) return message.channel.send("Put something to evaluate.");
     try {
-      let evaluated = await eval(args.slice(1).join(" "));
-      if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
-      const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
-   await message.channel.send(arr[0], { code: "js" });
+      let evaluated = eval(args.slice(1).join(" "));
+      if (evaluated instanceof Promise) {
+        const m = await message.channel.send("Evaluating promise...");
+        evaluated.then((e => {
+          let evaluated = e;
+          if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+          const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
+          m.edit(arr[0], { code: "js" });
+        }))
+      } else {
+        if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+        const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
+        await message.channel.send(arr[0], { code: "js" });
+      }
     } catch (err) {
       const arr = Discord.Util.splitMessage(err.toString(), { maxLength: 1950 });
-   await message.channel.send("```js\n" + arr[0] + "```");
+      await message.channel.send("```js\n" + arr[0] + "```");
     }
   }
 }
