5fe93a1b71671490a18202522392fbc19af9c96d
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-10-16 20:26:51 -0500

    No self-lock

--- src/commands/owner/asynceval.js
+++ src/commands/owner/asynceval.js
@@ -14,13 +14,22 @@ export default class extends Command {
   async run(bot, message, args) {
     if (!args[1]) return message.channel.send("Put something to evaluate.\nThis allows using async/await without complications\nRemember to put `return` or this will not return anything.");
     try {
-      let evaluated = await eval("(async () => { " + args.slice(1).join(" ") + "})();");
-      if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
-      const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
-   await message.channel.send(arr[0], { code: "js" });
+      eval("(async () => { " + args.slice(1).join(" ") + "})();").then(e => {
+        let evaluated = e;
+        if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+        const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
+        message.channel.send(arr[0], { code: "js" });
+      }).catch(e => {
+        let evaluated = e;
+        if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+        const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950 });
+        message.channel.send(arr[0], { code: "js" });
+      })
     } catch (err) {
-      const arr = Discord.Util.splitMessage(err.toString(), { maxLength: 1950 });
-   await message.channel.send("```js\n" + arr[0] + "```");
+      let cosa = err;
+      if (typeof cosa !== "string") cosa = util.inspect(cosa, { depth: 0 });
+      const arr = Discord.Util.splitMessage(cosa, { maxLength: 1950 });
+      message.channel.send(arr[0], { code: "js" });
     }
   }
 }
