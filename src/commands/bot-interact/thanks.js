58bc41360b5cea6973c7b45da933501e17de6696
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2020-10-31 20:06:15 -0500

    Update thanks.js

--- src/commands/bot-interact/thanks.js
+++ src/commands/bot-interact/thanks.js
@@ -9,9 +9,9 @@ export default class extends Command {
         const embed = new MessageEmbed()
         .attachFiles([att])
         .setTitle("Some of the people who helped in some way with this project =D")
-        .addField((await bot.users.fetch("455891977712566274")).tag, "Current bot and dashboard hosting.")
+        .addField((await bot.users.fetch("455891977712566274")).tag, "Current bot hosting.")
         .addField((await bot.users.fetch("398321973404368927")).tag, "https://gidget.xyz domain (1 year)")
-        .addField((await bot.users.fetch("224619540263337984")).tag, "Previous hosting on Heroku (so it's 24/7, not 22 days)")
+        .addField((await bot.users.fetch("224619540263337984")).tag, "Access to a free VPS :)")
         .addField((await bot.users.fetch("756733721171984444")).tag, "Support server creation and design")
         .addField("GitHub", "[Those who contributed to the bot code](https://github.com/AndreMor955/gidget/graphs/contributors)")
         .setImage("attachment://contributors.png");
