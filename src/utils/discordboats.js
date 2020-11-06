04521b79cb91cc93431e15236a2a7c8d6567d6fd
AndreMor955 <moralesandre75@gmail.com>
2020-11-05 21:03:34 -0500

    log

--- src/utils/discordboats.js
+++ src/utils/discordboats.js
@@ -2,5 +2,6 @@ import BOATS from 'boats.js';
 const Boats = new BOATS(process.env.DISCORDBOATS, "v2");
 export default async function(bot) {
     const posted = await Boats.postStats(bot.guilds.cache.size, bot.user.id);
+    console.log("discord.boats: Server count posted!");
     return posted;
 }
