00959c737c96debc44bfc85f19a8b02e53323294
AndreMor955 <moralesandre75@gmail.com>
2020-12-14 23:29:20 -0600

    Huh

--- src/utils/discordboats.js
+++ src/utils/discordboats.js
@@ -1,6 +1,6 @@
 import BOATS from 'boats.js';
-const Boats = new BOATS(process.env.DISCORDBOATS, "v2");
 export default async function(sharder) {
+    const Boats = new BOATS(process.env.DISCORDBOATS, "v2");
     const posted = await Boats.postStats((await sharder.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0), "694306281736896573");
     console.log("discord.boats: Server count posted!");
     return posted;
