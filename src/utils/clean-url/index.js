6c95c818c82d575dd30d480af10b89e540db3a75
AndreMor955 <moralesandre75@gmail.com>
2020-12-23 23:23:22 -0500

    Ok

--- src/utils/clean-url/index.js
+++ src/utils/clean-url/index.js
@@ -1,7 +1,5 @@
 import data from './assets/sites.json';
-import isporncallback from 'is-porn';
-import { promisify } from 'util';
-const isPorn = promisify(isporncallback);
+
 // methods to be exported
 export function checkCleanUrl(urlFromReq) {
     const parsedUrl = new URL(urlFromReq);
@@ -13,20 +11,5 @@ export function checkCleanUrl(urlFromReq) {
     if (check2 in data) return true;
     if (!host.startsWith('www.')) host = `www.${host}`;
     if (host in data) return true;
-    return false;
-}
-
-export async function checkSingleCleanURL(urlFromReq) {
-    const parsedUrl = new URL(urlFromReq);
-    let host = parsedUrl.host || parsedUrl.pathname.trim().split("/")[0];
-    if (host in data) return true;
-    const thing = host.split(".");
-    const check1 = thing.slice(thing.length - 2).join(".");
-    const check2 = "www." + check1;
-    if (check2 in data) return true;
-    if (!host.startsWith('www.')) host = `www.${host}`;
-    if (host in data) return true;
-    const cosas = await isPorn(check1);
-    if (cosas) return true;
     return false;
 }
