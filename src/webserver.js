b949ad39ad70fc299cca460116109931466b3123
AndreMor955 <moralesandre75@gmail.com>
2020-12-27 01:05:07 -0500

    hm

--- src/webserver.js
+++ src/webserver.js
@@ -4,10 +4,18 @@ import express from 'express';
  */
 export default function (sharder) {
   const app = express();
+  app.get("/ping", (req, res) => {
+    res.status(200).send("Good!");
+  });
   app.use((req, res, next) => {
     if (req.headers["pass"] !== process.env.ACCESS) {
       res.status(200).send("You don't have authorization");
     } else next();
+  });
+  app.get("/guilds", async (req, res) => {
+    const servers = await sharder.broadcastEval("this.guilds.cache.map(e => e.id)");
+    const merged = Array.prototype.concat.apply([], servers);
+    return res.json(merged);
   });
   app.get("/", async (req, res) => {
     try {
@@ -20,18 +28,12 @@ export default function (sharder) {
           res.status(404).send("Something's bad. Maybe server ID doesn't exist");
         }
       } else {
-        res.status(200).send("Good.");
+        res.status(200).send("Specify a server!");
       }
     } catch (err) {
       console.log(err);
       res.status(500).send("Something happened! " + err);
     }
-  });
-
-  app.get("/guilds", async (req, res) => {
-    const servers = await sharder.broadcastEval("this.guilds.cache.map(e => e.id)");
-    const merged = Array.prototype.concat.apply([], servers);
-    return res.json(merged);
   });
   const listener = app.listen(process.env.PORT, () => {
     console.log("Your app is listening on port " + listener.address().port);
