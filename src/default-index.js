bb4442c85c6b7bb166dcd37a551f088dad8778c3
AndreMor955 <moralesandre75@gmail.com>
2021-01-03 16:45:02 -0500

    I want to test this

--- src/default-index.js
+++ src/default-index.js
@@ -7,7 +7,7 @@ const manager = new ShardingManager('./src/default-bot.js', {
     token: process.env.DISCORD_TOKEN,
     totalShards: 2,
     //Change this depending on the machine.
-    execArgv: ["--experimental-json-modules", "--optimize_for_size", "--max_old_space_size=" + process.env.OLDMEMORY]
+    execArgv: ["--experimental-json-modules", "--expose-gc", "--optimize_for_size", "--max_old_space_size=" + process.env.OLDMEMORY]
 });
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn().then(() => {
