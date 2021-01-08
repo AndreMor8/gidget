0847e650f7b12bc434f751646f1ad2c9c04c4114
AndreMor955 <moralesandre75@gmail.com>
2021-01-08 15:30:58 -0500

    Changes

--- src/default-index.js
+++ src/default-index.js
@@ -3,11 +3,12 @@ dotenv.config();
 import webserver from './webserver.js';
 import discordboats from './utils/discordboats.js';
 import { ShardingManager } from 'discord.js';
+const execArgv = ["--experimental-json-modules", "--expose-gc"];
+if(process.env.OLDMEMORY) execArgv.push("--optimize_for_size", ("--max_old_space_size=" + process.env.OLDMEMORY));
 const manager = new ShardingManager('./src/default-bot.js', {
     token: process.env.DISCORD_TOKEN,
     totalShards: 2,
-    //Change this depending on the machine.
-    execArgv: ["--experimental-json-modules", "--expose-gc", "--optimize_for_size", "--max_old_space_size=" + process.env.OLDMEMORY]
+    execArgv
 });
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn().then(() => {
