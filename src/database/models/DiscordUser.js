f31582ffb5f7928999b70b6f0b89f7ad713f333b
AndreMor955 <55120710+AndreMor955@users.noreply.github.com>
2021-02-04 23:31:22 -0500

    Update DiscordUser.js

--- src/database/models/DiscordUser.js
+++ src/database/models/DiscordUser.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true },
     username: { type: String, required: true },
-    guilds: { type: Array, required: true },
     avatar: { type: String },
     premium_type: { type: Number }
 });
