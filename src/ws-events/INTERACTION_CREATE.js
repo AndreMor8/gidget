2912684f13cfbf127493c16c8359ab157c1f0590
AndreMor955 <moralesandre75@gmail.com>
2021-02-27 23:40:06 -0500

    ok

--- src/ws-events/INTERACTION_CREATE.js
+++ src/ws-events/INTERACTION_CREATE.js
@@ -3,9 +3,22 @@ export default async function (bot, interaction) {
         if (interaction.data.name === "wubbzy") {
             await bot.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
-                    type: 4,
+                    type: 3,
                     data: {
                         content: 'Wubbzy is the best! <a:WubbzyFaceA:612311062611492900>'
+                    }
+                }
+            })
+        }
+        if (interaction.data.name === "say") {
+            await bot.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        embeds: [{
+                            title: "Say command",
+                            description: interaction.data.options[0].value
+                        }]
                     }
                 }
             })
