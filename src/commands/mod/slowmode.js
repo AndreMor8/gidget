320a30cca3ca85ef4259cb84e861a582107edc38
AndreMor955 <moralesandre75@gmail.com>
2021-04-05 14:16:59 +0000

    YES

--- src/commands/mod/slowmode.js
+++ src/commands/mod/slowmode.js
@@ -13,7 +13,7 @@ export default class extends Command {
   async run(bot, message, args) {
     if (!args[1]) return message.channel.send('Usage: `slowmode <seconds> <channel>`');
     const number = parseInt(args[1]);
-    if(!number) return message.channel.send("Invalid number");
+    if((!number) && (number !== 0)) return message.channel.send("Invalid number");
     if (number > 21600 || number < 0) return message.channel.send('Invalid number. The number must be between 0 and 21600 seconds.');
     const channel = args[2] ? (message.guild.channels.cache.get(args[2]) || message.mentions.channels.first()) : message.channel;
     if (channel) await channel.edit({ rateLimitPerUser: number }, "Slowmode command").then(() => { message.channel.send('Slowmode changed to ' + number + ' seconds in ' + message.channel.toString()); }).catch(err => message.reply(`I couldn't change the slowmode. Here's a debug: ` + err));
