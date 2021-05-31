e19607062d99a48734574db854b1e20e9b0dbcc8
TheEaterOfSouls <74118796+TheEaterOfSouls@users.noreply.github.com>
2021-05-30 20:49:56 -0500

    Update nowplaying.js
    Espero sirva xd

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -1,5 +1,8 @@
 import moment from "moment";
 import "moment-duration-format";
+import { MessageEmbed } from "discord.js";
+import { splitBar } from "string-progressbar"; //npm i string-progressbar
+
 export default class extends Command {
   constructor(options) {
     super(options);
@@ -8,10 +11,35 @@ export default class extends Command {
     this.guildonly = true;
   }
   async run(bot, message) {
-    const serverQueue = message.guild.queue
+    const serverQueue = message.guild.queue;
     if (!serverQueue) return message.channel.send("There is nothing playing.");
     if (!serverQueue.connection) return;
     if (!serverQueue.connection.dispatcher) return;
-    await message.channel.send(`Now playing: **${serverQueue.songs[0].title}**\nTime: ${moment.duration(serverQueue.connection.dispatcher.streamTime + (serverQueue.songs[0].seektime * 1000), "ms").format()} / ${moment.duration(serverQueue.songs[0].duration, "seconds").format()}`);
+
+    const embed_success = new MessageEmbed()
+      .setDescription([
+        `Now playing: **[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})**`,
+        `Time: ${moment
+          .duration(
+            serverQueue.connection.dispatcher.streamTime +
+              serverQueue.songs[0].seektime * 1000,
+            "ms"
+          )
+          .format()} / ${moment
+          .duration(serverQueue.songs[0].duration, "seconds")
+          .format()}`,
+        `Progress Bar:\n**[${
+          splitBar(
+            serverQueue.songs[0].duration,
+            serverQueue.connection.dispatcher.streamTime +
+              serverQueue.songs[0].seektime * 1000,
+            15,
+            "▬",
+            "<a:KicketyKickBallA:649698360978178061>"
+          )[0]
+        }]**`,
+      ])
+      .setColor(0xffff00);
+    await message.channel.send({ embed: embed_success });
   }
 }
