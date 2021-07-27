ee00fb40526baeecdad3854da726f8c4ec32695f
AndreMor8 <moralesandre75@gmail.com>
2021-07-27 15:25:18 +0000

    si

--- src/commands/image/titlecard.js
+++ src/commands/image/titlecard.js
@@ -1,6 +1,6 @@
-import { mwn } from 'mwn';
+import fetch from 'node-fetch';
 import { MessageEmbed } from 'discord.js';
-
+let pages;
 export default class extends Command {
     constructor(options) {
         super(options);
@@ -12,29 +12,25 @@ export default class extends Command {
         }
     }
     async run(bot, message, args) {
-        const mediabot = new mwn({
-            apiUrl: 'https://wubbzy.fandom.com/api.php',
-            userAgent: `GidgetDiscordBot ${bot.botVersion}`
-        });
-        await mediabot.getSiteInfo();
-        const pages = (await mediabot.getPagesInCategory("Category:Episodes", { cmtype: "page" })).filter(e => !e.includes("/"));
+        if (!pages) {
+            const res = await fetch("https://wubbzy.fandom.com/api.php?action=query&format=json&prop=&list=categorymembers&meta=&formatversion=2&cmtitle=Category%3AEpisodes&cmprop=title&cmtype=page&cmlimit=250");
+            pages = (await res.json()).query.categorymembers.filter(e => !e.title.includes("/")).map(e => e.title);
+        }
         const page = pages.find(e => {
             const to = e.replace("(episode)", "").trimEnd();
             return to.toLowerCase() === args.slice(1).join(" ").toLowerCase() || to.replace("!", "").toLowerCase() === args.slice(1).join(" ").toLowerCase();
         });
-        if(!args[1] || !page) {
-           const embed = new MessageEmbed()
-           .setDescription(pages.map(e => e.replace("(episode)", "").trimEnd()).join("\n"))
-           .setTitle("List of episodes");
-           return await message.channel.send({embeds: [embed]});
+        if (!args[1] || !page) {
+            const embed = new MessageEmbed()
+                .setDescription(pages.map(e => e.replace("(episode)", "").trimEnd()).join("\n"))
+                .setTitle("List of episodes");
+            return await message.channel.send({ embeds: [embed] });
         }
-        const res = await mediabot.request({
-            "action": "imageserving",
-            "wisTitle": page,
-        });
+        const r2 = await fetch(`https://wubbzy.fandom.com/api.php?action=imageserving&format=json&wisTitle=${encodeURIComponent(page)}&formatversion=2`)
+        const res2 = await r2.json();
         const embed = new MessageEmbed()
-        .setImage(`${res.image.imageserving}&format=original`)
-        .setTitle(`${page.replace("(episode)", "").trimEnd()}'s title card`);
-        await message.channel.send({embeds: [embed]});
+            .setImage(`${res2.image.imageserving}&format=original`)
+            .setTitle(`${page.replace("(episode)", "").trimEnd()}'s title card`);
+        await message.channel.send({ embeds: [embed] });
     }
 }
