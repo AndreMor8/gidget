7ba000af48172bd2d6640ee037d0143b894285df
AndreMor8 <moralesandre75@gmail.com>
2021-08-23 05:50:34 +0000

    A

--- src/slashcommands/music/queue.js
+++ src/slashcommands/music/queue.js
@@ -18,7 +18,7 @@ export default class extends SlashCommand {
     const contents = Util.splitMessage(interaction.options.getBoolean("previous", false) ? `${queue.previousSongs.reverse().map((song, i) => `**${parseInt(i) + 1}** ${song.name} (${song.formattedDuration})`).join(`\n`)}` : `${queue.songs.map((song, i) => `**${i}** ${song.name} (${song.formattedDuration})`).join(`\n`)}`, { maxLength: 600 });
     const embeds = [];
     for (const i in contents) {
-      embeds.push(new MessageEmbed().setTitle(i == 1 ? (interaction.options.getBoolean("previous") ? "Previous songs" : "Song queue") : "").setDescription(contents[i]).setColor("RANDOM"));
+      embeds.push(new MessageEmbed().setTitle(i == 0 ? (interaction.options.getBoolean("previous") ? "Previous songs" : "Song queue") : "").setDescription(contents[i]).setColor("RANDOM"));
       if (i == 9) break;
     }
     return await interaction.reply({ content: `Total duration: **${queue.formattedDuration}**\n\n**Now playing:** ${queue.songs[0].name} (${queue.formattedCurrentTime} / ${queue.songs[0].formattedDuration})`, embeds, ephemeral: true });
